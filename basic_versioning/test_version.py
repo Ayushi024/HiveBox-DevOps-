7e7b486469edc37aa505ebd65ae3e1df12c6fa33
Ayushi Panwar <ayushi982@gmail.com>
2025-02-23 14:41:08 +0530

    edited

--- basic_versioning/test_version.py
+++ basic_versioning/test_version.py
@@ -8,7 +8,7 @@ import subprocess
 def test_version():
     """Test that app.py prints the correct version."""
     result = subprocess.run(
-        ["python", "basic-versioning/app.py"], capture_output=True, text=True
+        ["python", "basic_versioning/app.py"], capture_output=True, text=True
     )
     assert result.returncode == 0
     assert "App Version: v0.0.1" in result.stdout
