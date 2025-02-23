29d36cfa6250c1b206fc540101ff0aac33a39031
Ayushi Panwar <ayushi982@gmail.com>
2025-02-23 17:30:18 +0530

    edited

--- basic_versioning/app.py
+++ basic_versioning/app.py
@@ -36,4 +36,5 @@ def get_version():
 
 if __name__ == "__main__":
     # Run the Flask application on port 5001, accessible from any network interface.
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="127.0.0.1", port=5001)
+
